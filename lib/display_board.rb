5fbd12298ee6c95f6f4eee36f3ff7fa32e7aefc1
Pratik <pr.tikgohil@gmail.com>
2020-02-19 10:06:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board arg
-  puts arg
+  puts "#{arg}++"
 end
 display_board ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
