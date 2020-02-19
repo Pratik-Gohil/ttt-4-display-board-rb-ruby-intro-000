a0ce583104722d038a5cc764d0f928184ca2fdde
Pratik <pr.tikgohil@gmail.com>
2020-02-19 10:08:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,5 +3,7 @@
 def display_board arg
   puts " #{arg[0]} | #{arg[0]} | #{arg[0]} "
   puts "-----------"
+  puts " #{arg[0]} | #{arg[0]} | #{arg[0]} "
+  puts " #{arg[0]} | #{arg[0]} | #{arg[0]} "
 end
 display_board ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
