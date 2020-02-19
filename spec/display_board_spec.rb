5a56b7dfa3c1b10e99130f999f1b1db50b40f7df
Pratik <pr.tikgohil@gmail.com>
2020-02-19 10:12:53 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -183,6 +183,12 @@ describe "#display_board in 'lib/display_board.rb" do
       # and make a few simple edits to convert the previous example to this
       # example's situation.
 
+      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("   |   |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("   |   |   ")
+
       # *** Comment the line below by adding a # at the line start ***
       expect(true).to be(true)
     end
